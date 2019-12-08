9f7cb118929ce88cde4ccf63060e31c6a00b1200
kristopherpesci <kristopher.pesci@gmail.com>
2019-12-08 19:47:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,10 +2,6 @@
 def valid_move? (board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     true
-  elseif
-    false
-elseif index.between?(0, 8)
-  true
 else
   false
 end
