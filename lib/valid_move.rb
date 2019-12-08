c3f2caf29599f71672059c2295a744b80e5e3e72
kristopherpesci <kristopher.pesci@gmail.com>
2019-12-08 19:38:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
-  if (board[index] == " " || board[index] == "" || board[index] == nil && board[index] ==.between?(0,8))
+  if (board[index] == " " || board[index] == "" || board[index] == nil && board[index].between?(0,8))
     true
   else
     false
